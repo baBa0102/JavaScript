81e54e5b5b3729545b2cfb473dbd9e9760ed3f2b
Basit Shameem <clashofclansboss796@gmail.com>
2026-08-27 11:08:39 +0530

    nested loops, squares

--- loops.js
+++ loops.js
@@ -4,4 +4,16 @@ for(let i = 1; i<=5; i++){
 for (let i = 10; i<=9; i++){
     console.log(i);
 }
-console.log("Loop exited")
+console.log("Loop exited");
+
+console.log("Squares from 2 to 20");
+for (let i = 2; i<= 20; i++){
+    console.log(i*i);
+}
+
+// ask for number and provide it's table;
+console.log("multiplication table ");
+let n = 45;
+for(let i = n; i<= n*10; i= i+n){
+    console.log(i);
+}
